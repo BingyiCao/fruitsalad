88093b167767247cb6ec758c48bb1cc86d853928
yydcool <yydcool@gmail.com>
2013-11-10 11:17:54 -0500

    update2

--- Test.bat
+++ Test.bat
@@ -1,2 +1,2 @@
-java fruit.sim.Fruit players.list 12 power2.txt false false 1000 1>out.txt 2>err.txt
+java fruit.sim.Fruit ./players/20players.txt 12 power2.txt false false 10 1>out.txt 2>err.txt
 pause
